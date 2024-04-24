60c8fed49345cde923fe4356f2c5121eae38758d
zippy-dice <zippy_dice@proton.me>
2024-04-24 18:21:29 +0900

    Use Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    #[arg(long = "input")]
+    input_file: String,
+    #[arg(long = "output")]
+    output_file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{}", cli.input_file);
+    println!("{}", cli.output_file.unwrap_or("No File".into()));
 }
